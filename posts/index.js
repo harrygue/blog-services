e6a2bf2b6fc32c4dd55d994e4864ba34b471ebbf
harrygue <hg070817.gue@gmail.com>
2021-05-25 08:14:24 +0200

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const {randomBytes} = require('crypto')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const posts = {}
+
+app.get('/posts',(req,res)=>{
+    posts && console.log(posts)
+    res.send(posts)
+
+})
+
+app.post('/posts',(req,res)=>{
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+        id,title
+    }
+
+    res.status(201).send(posts[id])
+})  
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
+})
